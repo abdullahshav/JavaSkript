db542a8eae44d42510b11551f14eb5d7d741064f
Yulduzxon <alisherz1980@gmail.com>
2023-09-25 00:25:52 +0500

    added js file

---
+++ app.js
@@ -0,0 +1,18 @@
+let firstNum = prompt("Birinchi son");
+
+// let secondNum = prompt("ikkinchi son");
+
+
+
+if (firstNum < 35) {
+    console.log("firstNum");
+
+} else if (firstNum >= 35 && firstNum < 45) {
+    console.log("secondNum");
+} else if (firstNum >= 45 && firstNum < 65) {
+    console.log("thirdNum");
+} else if (firstNum > 65) {
+    console.log("Zero");
+} else {
+    console.log("Error");
+}
